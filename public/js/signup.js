88a66ff45dd244a22b0fa202615f7b54af4211db
CJ237 <79546904+CJ237@users.noreply.github.com>
2024-10-02 00:07:49 -0500

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,12 +2,13 @@ const signupFormHandler = async function(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-input-signup').value.trim();
+    const email = document.querySelector('#email-input-signup').value.trim();
     const password = document.querySelector('#password-input-signup').value.trim();
     
     const response = await fetch('/api/user', {
       method: 'POST',
       body: JSON.stringify({
-        username, password
+        username, email, password
       }),
       headers: { 'Content-Type': 'application/json' },
     });
